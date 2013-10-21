0e1e33874613e57ca175c144b1e3338b61caeb94
Student <user@chromebox-004.(none)>
2013-10-21 16:29:53 -0700

    hey everything works

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app.secret_key = "shhhhthisisasecret"
 # / has two routing functions. this one is for GET. the form has no action URL so it will just reload the same page it is on.
 @app.route("/")
 def index():
+    username = logged_in()
+    if username:
+        return redirect(url_for("show_wall_posts", username=username))
+
     return render_template("index.html")
 
 # this routing function gets called after you submit the form on /.
@@ -28,27 +32,83 @@ def process_login():
         flash("Password incorrect, please try again.")
 
     # go to the index page once we are done trying to authenticate. url_for allows us to change our URLs later without breaking all of the references to them.
-    return redirect(url_for("index"))
+    return redirect(url_for("show_wall_posts", username=username))
 
 @app.route("/register")
 def register():
+    username = logged_in()
+    if username:
+        return redirect(url_for("show_wall_posts", username=username))
+        
     return render_template("register.html")
 
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(url_for("index"))
+@app.route("/register", methods=['POST'])
+def create_account():
+    username = logged_in()
+    if username:
+        return redirect(url_for("show_wall_posts", username=username))
+
+    new_username = request.form.get("username")
+    new_pwd = request.form.get("password")
+    verify_pass = request.form.get("password_verify")
+    model.connect_to_db()
+
+    existing_user = model.get_user_by_name(new_username)
+
+    if existing_user:
+        flash("Username already exists, please try again.")
+        return redirect(url_for("register"))
+
+    if new_pwd == verify_pass:
+        model.create_user(new_username, new_pwd)
+        flash("User created! You can now log in.")
+        return redirect(url_for("index"))
+    else:
+        flash("Passwords did not match.")
+        return redirect(url_for("register"))
 
 @app.route("/user/<username>")
 def show_wall_posts(username):
     model.connect_to_db()
     display_id = model.get_user_by_name(username)
     posts = model.get_posts_by_user_id(display_id)
+    pretty_data = []
+
+    # post: 0: owner ID, 1: author ID, 2: date, 3: text
+    for post in posts:
+        pretty_data.append( (post[0], model.get_name_by_id(post[1]), post[2], post[3]) )
+
+    user_id = session.get('user_id')
+    profile = username
+    if user_id:
+        return render_template("wall.html", posts=pretty_data, userid=user_id, username=profile)
+
+    return render_template("wall.html", posts=pretty_data, profile=username)
+
+@app.route("/user/<username>", methods=['POST'])
+def post_to_wall(username):
+    content = request.form.get("post_text")
+    # the person writing the post is the person logged in
+    author_id = session['user_id']
+    model.connect_to_db()
+    # the owner is the person's wall we are currently looking at, and then we need the ID of that person
+    print "username is", username
+    owner_id = model.get_user_by_name(username)
+    # insert a row into the DB that contains the relevant data
+    model.write_wall_post(owner_id, author_id, content)
+    # then render the page with all of the wall posts on it, including the one we just wrote.
+    return redirect(url_for("show_wall_posts", username=username))
+ 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+def logged_in():
     user_id = session.get('user_id')
     if user_id:
-        return render_template("wall.html", posts=posts, userid=user_id)
+        model.connect_to_db()
+        return model.get_name_by_id(user_id)
 
-    return render_template("wall.html", posts=posts)
- 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -19,12 +19,30 @@ def get_user_by_name(username):
     query = """SELECT id FROM Users WHERE username = ?"""
     DB.execute(query, (username,))
     row = DB.fetchone()
+    if row:
+        return row[0]
+    return row
+
+def get_name_by_id(user_id):
+    query = """SELECT username FROM Users WHERE id = ?"""
+    DB.execute(query, (user_id,))
+    row = DB.fetchone()
     return row[0]
 
 def get_posts_by_user_id(user_id):
-    query = """SELECT id, owner_id, author_id, created_at, content FROM Wall_Posts WHERE owner_id = ?"""
+    query = """SELECT owner_id, author_id, created_at, content FROM Wall_Posts WHERE owner_id = ?"""
     DB.execute(query, (user_id,))
     row = DB.fetchall()
     return row #this is a list
+
+def write_wall_post(owner_id, author_id, post_text):
+    query = """INSERT into Wall_Posts (owner_id, author_id, created_at, content) values (?, ?, datetime('now'), ?) """
+    DB.execute(query, (owner_id, author_id, post_text))
+    CONN.commit()
+
+def create_user(username, password):
+    query = """INSERT into Users (username, password) values (?, ?) """
+    DB.execute(query, (username, hash(password)))
+    CONN.commit()
 
 connect_to_db()
