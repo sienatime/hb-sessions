28760b9367c5928d26cc02fe1863275237ebd4a6
Student <user@chromebox-004.(none)>
2013-10-21 13:01:30 -0700

    makin the fb

--- app.py
+++ app.py
@@ -4,17 +4,51 @@ import model
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
 
+# / has two routing functions. this one is for GET. the form has no action URL so it will just reload the same page it is on.
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# this routing function gets called after you submit the form on /.
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    # calls the function authenticate() in model.py and returns True if user authentication info matches, and False if it does not.
+    model.connect_to_db()
+    authenticated = model.authenticate(username, password)
+    if authenticated:
+        # a flash is just a message that we can show to the user once
+        flash("User authenticated!")
+        # saves the username in the session so we can keep using it
+        user_id = model.get_user_by_name(username)
+        session['user_id'] = user_id
+    else:
+        flash("Password incorrect, please try again.")
+
+    # go to the index page once we are done trying to authenticate. url_for allows us to change our URLs later without breaking all of the references to them.
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+@app.route("/user/<username>")
+def show_wall_posts(username):
+    model.connect_to_db()
+    display_id = model.get_user_by_name(username)
+    posts = model.get_posts_by_user_id(display_id)
+    user_id = session.get('user_id')
+    if user_id:
+        return render_template("wall.html", posts=posts, userid=user_id)
+
+    return render_template("wall.html", posts=posts)
+ 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,30 @@
-ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+import sqlite3
+
+DB = None
+CONN = None
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
 
 def authenticate(username, password):
-    return False
+    query = """SELECT id FROM Users WHERE username = ? AND password = ?"""
+    DB.execute(query, (username, hash(password)))
+    row = DB.fetchone()
+    
+    return row
+
+def get_user_by_name(username):
+    query = """SELECT id FROM Users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+    return row[0]
+
+def get_posts_by_user_id(user_id):
+    query = """SELECT id, owner_id, author_id, created_at, content FROM Wall_Posts WHERE owner_id = ?"""
+    DB.execute(query, (user_id,))
+    row = DB.fetchall()
+    return row #this is a list
+
+connect_to_db()
